8324a59aa9dc062edc48ef6327f5187fa78c2006
Algor <31491210+jaimeluengo@users.noreply.github.com>
2018-02-04 19:45:45 -0500

    Group B tested

--- BasicTest.java
+++ BasicTest.java
@@ -5,14 +5,25 @@ import org.junit.jupiter.api.Test;
 class PhDTest {
 
 	@Test
-	void test1() {
-		PhD john= new PhD("Jaime Luengo",4,2019);
-		assert("Jaime Luengo",john.name());
-		assert(4,john.month());
-		assert(2019,john.year());
-		assert(null,john.advisor1());
-		assert(null,john.advisor2());
-		assert(0,john.numAdvisees());
+	void testGroupA() {
+		PhD jaime= new PhD("Jaime Luengo",12,2019);
+		assert("Jaime Luengo"==jaime.name());
+		assert(12==jaime.month());
+		assert(2019==jaime.year());
+		assert(null==jaime.advisor1());
+		assert(null==jaime.advisor2());
+		assert(0==jaime.numAdvisees());
+	}
+	
+	@Test
+	void testGroupB() {
+		PhD Lebron= new PhD("Lebron James",11,1995);
+		PhD David= new PhD("David Gries",1,1973);
+		PhD jaime= new PhD("Jaime Luengo",12,2019);
+		jaime.setAdvisor1(Lebron);
+		jaime.setAdvisor2(David);
+		assertEquals(jaime.advisor1(),Lebron);
+		assertEquals(jaime.advisor2(),David);
 	}
 
 }
